501be1a5575d1e69886913da1c3870252653ffd0
zhuoIC <nicolimonar@gmail.com>
2017-10-09 14:42:42 +0200

    Pa casa

---
+++ app/src/main/java/com/example/inventory/InventoryActivity.java
@@ -0,0 +1,12 @@
+package com.example.inventory;
+
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class InventoryActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/example/inventory/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.inventory;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextView txvSignUp;
+    private Button btnSignIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        txvSignUp = (TextView) findViewById(R.id.txvSingUp);
+        //txvSignUp.setPaintFlags(txvSignUp.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
